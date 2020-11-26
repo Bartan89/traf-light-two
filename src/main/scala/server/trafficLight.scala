091c9c303b80c0c2eeb96c453b97e8a9b9aa1c3d
bartan89 <bart.kuijper89@gmail.com>
2020-11-26 15:35:46 +0100

    ignore more

--- src/main/scala/server/trafficLight.scala
+++ src/main/scala/server/trafficLight.scala
@@ -85,9 +85,11 @@ object TrafficLights extends App with JsonSupport {
     }
 
 
-  val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
+  val PORT = 8060
 
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  val bindingFuture = Http().newServerAt("localhost",  port =PORT).bind(routes)
+
+  println(s"Server online at http://localhost:${PORT}/\nPress RETURN to stop...")
   StdIn.readLine()
   bindingFuture
     .flatMap(_.unbind())
